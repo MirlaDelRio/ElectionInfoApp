b747513ca09abcd40691cab8a346794183adb009
mirladelrio <mirladelrio@gmail.com>
2016-05-08 21:18:21 -0400

    init

---
+++ ElectoralSystem/Controllers/CandidatesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElectoralSystem.Controllers
+{
+    public class CandidatesController : Controller
+    {
+        // GET: CandidatesMap
+        public ActionResult Map()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
